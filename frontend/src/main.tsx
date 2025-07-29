86eed8677d64eb2e00e92e13677f89063d6bc493
PranavKrishnaS <pksckochi@gmail.com>
2025-07-29 09:20:11 +0530

    initial setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './App'
+import { Provider } from './components/ui/provider'
+import React from 'react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 )
